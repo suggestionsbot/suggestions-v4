7fdd36adaa4f0e1aa2cce4f78b6bcb369b3b0677
skelmis <git@post.org.nz>
2026-07-05 00:05:13 +1200

    attempt to fix configs

--- shared/utils/configs.py
+++ shared/utils/configs.py
@@ -17,17 +17,57 @@ async def ensure_guild_config(guild_id: int) -> GuildConfigs:
     if gc._was_created:
         logger.debug("Created new GuildConfig for %s", guild_id)
 
-    return gc
+    # return gc
+    pgc_insert = (
+        await PremiumGuildConfigs.insert(
+            PremiumGuildConfigs(guild_id=guild_id),
+        )
+        .on_conflict(action="DO NOTHING", target=(PremiumGuildConfigs.guild_id,))
+        .returning(*PremiumGuildConfigs.all_columns())
+    )
+    if not pgc_insert:
+        pgc_insert = (
+            await PremiumGuildConfigs.objects()
+            .first()
+            .where(PremiumGuildConfigs.guild_id == guild_id)
+        )
+    else:
+        # need object for reference reasons
+        pgc_insert = PremiumGuildConfigs(**pgc_insert[0])
+        pgc_insert._exists_in_db = True
+
+    try_insert = (
+        await GuildConfigs.insert(
+            GuildConfigs(guild_id=guild_id, premium=pgc_insert),
+        )
+        .on_conflict(action="DO NOTHING", target=(GuildConfigs.guild_id,))
+        .returning(*GuildConfigs.all_columns())
+    )
+    if try_insert:
+        # New object
+        logger.debug("Created new GuildConfigs for %s", guild_id)
+        obj = GuildConfigs(**try_insert[0])
+        obj._exists_in_db = True
+        return obj
+
+    return await GuildConfigs.objects().first().where(GuildConfigs.guild_id == guild_id)
 
 
 async def ensure_user_config(
     user_id: int, *, locale: hikari.Locale | str = hikari.Locale.EN_GB
 ) -> UserConfigs:
-    uc: UserConfigs = await UserConfigs.objects().get_or_create(
-        UserConfigs.user_id == user_id,
-        defaults={UserConfigs.user_id: user_id, UserConfigs.primary_language_raw: locale},
+    try_insert = (
+        await UserConfigs.insert(
+            UserConfigs(user_id=user_id, primary_language_raw=locale),
+        )
+        .on_conflict(action="DO NOTHING", target=(UserConfigs.user_id,))
+        .returning(*UserConfigs.all_columns())
     )
-    if uc._was_created:
+    if try_insert:
+        # New object
         logger.debug("Created new UserConfig for %s", user_id)
+        obj = UserConfigs(**try_insert[0])
+        obj._exists_in_db = True
+        return obj
 
-    return uc
+    return await UserConfigs.objects().first().where(UserConfigs.user_id == user_id)
